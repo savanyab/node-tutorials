ff6decf51b923adb7efdbdbde5c309851dc47baf
Savanya Brigitta <savanyab@gmail.com>
2019-07-10 13:44:26 +0200

    fix function in query

--- 5-mongoDb/2-querying-for-data.js
+++ 5-mongoDb/2-querying-for-data.js
@@ -5,7 +5,7 @@ MongoClient.connect(url, function (err, client) {
   var db = client.db('EmployeeDB');
   var cursor = db.collection('Employee').find();
   
-  cursor.forEach(function (err, doc) {
+  cursor.forEach(function (doc) {
     console.log(doc);
   });
 }); 
