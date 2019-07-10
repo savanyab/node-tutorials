f9a886c28544bcc641e0480af93b5a0769109273
Savanya Brigitta <savanyab@gmail.com>
2019-07-10 11:35:43 +0200

    add mongoose getting started examples

---
+++ mongoose-examples/getting-started.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected')
+});
+
+var kittySchema = new mongoose.Schema({
+  name: String
+});
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+
+var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name);
+
+var fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak();
+
+Kitten.find(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens);
+})
+
+Kitten.find({ name: /^fluff/ }, callback);
